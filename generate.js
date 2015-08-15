662bd77416cf80ad97d7198cfc340bc21d65a037
Andrew Kiellor <akiellor@gmail.com>
2015-08-14 23:11:59 -0700

    Statistical approach working decently.

--- generate.js
+++ generate.js
@@ -1,14 +1,14 @@
 var escodegen = require('escodegen');
 var defaultStatistics = require(__dirname + '/statistics.json');
 
-function buildCdf(stats) {
-  var result = {};
+function buildCumulativeDistribution(stats) {
+  var result = {}
   Object.keys(stats).forEach(function(key) {
     result[key] = result[key] || [];
     Object.keys(stats[key]).forEach(function(childType, idx) {
-      result[key][childType] = result[key][childType] || [];
+      childType = '' + childType;
       var previous = result[key][idx - 1];
-      var sum = (previous && previous.range[1] || 0);
+      var sum = (previous && previous.range && previous.range[1] || 0);
       var count = stats[key][childType];
       result[key].push({
         type: childType,
@@ -20,86 +20,79 @@ function buildCdf(stats) {
   return result;
 }
 
-function generator(name, blueprint, extras) {
-  extras = extras || {};
-  var deps = [];
-  var props = [];
-  for (var prop in blueprint) {
-    var parts = blueprint[prop].split(':');
-    deps.push(parts[parts.length - 1]);
-    props.push({
-      collection: parts.length > 1,
-      name: prop,
-      type: parts[parts.length - 1]
-    });
-  }
-  deps.push(function() {
-    var result = {};
-    var args = Array.prototype.slice.call(arguments);
-    result.type = name;
-    args.forEach(function(e, i) {
-      var prop = props[i];
-      if (prop.collection) {
-        result[prop.name] = [e];
-      } else {
-        result[prop.name] = e;
+function buildPropertyModel(stats) {
+  var result = {};
+  Object.keys(stats).forEach(function(key) {
+    var parts = key.split('.');
+    for (var i = 0; i < parts.length; i+=2) {
+      var type = parts[i];
+      var prop = parts[i + 1];
+      result[type] = result[type] || [];
+      if (result[type].indexOf(prop) === -1) {
+        result[type].push(prop);
       }
-    });
-    Object.keys(extras).forEach(function(key) {
-      result[key] = extras[key];
-    });
-    return result;
+    }
   });
-  return {name: name, deps: deps, props: props};
+  return result;
 }
 
-var gens = [
-  generator('Identifier', {}, {name: 'foo'}),
-  generator('Literal', {}, {value: 40}),
-  generator('VariableDeclarator', {id: 'Identifier'}),
-  generator('VariableDeclaration', {declarations: 'list:VariableDeclarator'}, {kind: 'var'}),
-  generator('BlockStatement', {body: "list:Statement"}),
-  generator('FunctionDeclaration', {id: "Identifier", params: "list:Identifier", body: "BlockStatement"}),
-  generator('ArrayExpression', {elements: "list:Expression"}),
-  generator('WhileStatement', {test: 'Expression', body: 'BlockStatement'}),
-  generator('BinaryExpression', {left: "Expression", right: "Expression"}, {operator: '==='}),
-  generator('ExpressionStatement', {expression: "Expression"}),
-  generator('ThisExpression', {}),
-  generator('MemberExpression', {object: "Expression", property: "Identifier"}),
-  generator('CallExpression', {callee: "Expression", arguments: "list:Expression"}),
-  generator('AssignmentExpression', {left: "Identifier", right: "Expression"}, {operator: '='}),
-  generator('FunctionExpression', {body: "BlockStatement", params: "list:Identifier"}),
-  generator('ObjectExpression', {properties: "list:Property"}),
-  generator('Property', {key: "Identifier", value: "Expression"}),
-  generator('ReturnStatement', {argument: "Expression"}),
-  generator('ForInStatement', {left: "Identifier", right: "Expression", body: "BlockStatement"}),
-  generator('ThrowStatement', {argument: "Expression"}),
-  generator('UpdateExpression', {argument: 'Identifier'}, {operator: '++'}),
-  generator('ForStatement', {init: 'VariableDeclaration', test: 'BinaryExpression', update: 'UpdateExpression', body: 'BlockStatement'}),
-  generator('IfStatement', {test: 'BinaryExpression', consequent: 'BlockStatement'}),
-  generator('UnaryExpression', {argument: 'Expression'}, {operator: '+'}),
-  generator('LogicalExpression', {left: 'Expression', right: 'Expression'}, {operator: '&&'}),
-  generator('ConditionalExpression', {test: 'Expression', consequent: 'Expression'}, {}),
-  generator('BreakStatement', {}, {}),
-  generator('NewExpression', {callee: 'Identifier', arguments: 'list:Expression'}, {}),
-  generator('SequenceExpression', {}, {})
-];
+function sample(stats) {
+  var sum = stats[stats.length - 1].range[1];
+  var idx = Math.floor(Math.random() * sum) + 1;
+  return stats.filter(function(entry) {
+    return entry.range[0] < idx && idx <= entry.range[1];
+  })[0];
+}
 
-function generate(type, statistics) {
-  var gen = gens.filter(function(gen) { return gen.name === type; })[0];
-  var deps = gen.props.map(function(prop) {
-    var stats = statistics[type + '.' + prop.name];
-    var sum = stats[stats.length - 1].range[1];
-    var idx = Math.floor(Math.random() * sum) + 1;
-    var entry = stats.filter(function(entry) {
-      return entry.range[0] < idx && idx <= entry.range[1];
-    })[0];
-    return generate(entry.type, statistics);
-  });
-  return gen.deps[gen.deps.length - 1].apply(null, deps);
+function newGenerate(path, statistics, propertyModel) {
+  var type = path[path.length - 1];
+  var props = propertyModel[type] || [];
+  var deps = props.reduce(function(memo, prop) {
+    var propPath = path.concat([prop]);
+    var stats = statistics[propPath.join('.')];
+    if (!stats) {
+      return memo;
+    }
+    var entry = sample(stats);
+
+    if (type === 'Literal' && prop === 'raw') {
+      memo[prop] = entry.type;
+    } else if (type === 'Literal' && prop === 'value') {
+    } else if (type === 'Identifier' && prop === 'name') {
+      memo[prop] = entry.type;
+    } else if (prop === 'operator') {
+      memo[prop] = entry.type;
+    } else if (prop === 'kind') {
+      memo[prop] = entry.type;
+    } else if (prop.indexOf('[]') !== -1) {
+      var childPath = propPath.slice(-4)
+      childPath = childPath.concat([entry.type]);
+      memo[prop.replace('[]', '')] = [
+        newGenerate(childPath, statistics, propertyModel)
+      ];
+    } else {
+      var childPath = propPath.slice(-4)
+      childPath = childPath.concat([entry.type]);
+      memo[prop] = newGenerate(childPath, statistics, propertyModel);
+    }
+    return memo;
+  }, {});
+  deps.type = type;
+  if (type === 'Literal') {
+    var value = require('esprima').parse(deps.raw).body[0].expression.value;
+    deps.value = value;
+  }
+  return deps;
 }
 
 module.exports = function(type, rawStatistics) {
-  var statistics = buildCdf(rawStatistics || defaultStatistics);
-  return escodegen.generate(generate(type, statistics));
+  var propertyModel = buildPropertyModel(rawStatistics || defaultStatistics);
+  var statistics = buildCumulativeDistribution(rawStatistics || defaultStatistics, propertyModel);
+  return escodegen.generate(
+    newGenerate([type], statistics, propertyModel),
+    {
+      parse: require('esprima').parse,
+      raw: true
+    }
+  );
 };
