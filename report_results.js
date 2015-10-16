e2bdbd5be1df7b9577a22d1add8766eca67dedb1
Andrew Kiellor <akiellor@gmail.com>
2015-10-16 00:06:02 -0700

    Fail command if threshold exceeded.

--- report_results.js
+++ report_results.js
@@ -1,5 +1,5 @@
-module.exports = function reportResults(results) {
-  return Object.keys(results).reduce(function(memo, key) {
+module.exports = function reportResults(allowableFailures, results) {
+  var display = Object.keys(results).reduce(function(memo, key) {
     memo += key + '\n';
     memo += results[key].source + '\n';
     results[key].locations.forEach(function(file) {
@@ -7,4 +7,9 @@ module.exports = function reportResults(results) {
     });
     return memo;
   }, '');
+
+  var numberOfDuplications = Object.keys(results).length;
+  var exitCode = (numberOfDuplications <= allowableFailures || allowableFailures === -1) ? 0 : 1;
+
+  return {data: display, exitCode: exitCode};
 };
--- test/report_results_spec.js
+++ test/report_results_spec.js
@@ -3,7 +3,7 @@ var reportResults = require(__dirname + '/../report_results');
 
 describe('report results', function() {
   it('should print results nicely', function() {
-    var display = reportResults({
+    var output = reportResults(-1, {
       '123': {
         hash: '123',
         children: [],
@@ -18,7 +18,8 @@ describe('report results', function() {
       }
     });
 
-    expect(display).to.equal(
+    expect(output.exitCode).to.equal(0);
+    expect(output.data).to.equal(
       '123\n' +
       'var a = 1;\n' +
       'foo.js:1\n' +
@@ -27,5 +28,31 @@ describe('report results', function() {
       'var b = 1;\n' +
       'foo.js:2\n'
     );
+  });
+
+  it('should report bad exit code if too many duplications', function() {
+    var output = reportResults(0, {
+      '123': {
+        hash: '123',
+        children: [],
+        locations: ['foo.js:1', 'bar.js:2'],
+        source: 'var a = 1;'
+      }
+    });
+
+    expect(output.exitCode).to.equal(1);
+  });
+
+  it('should not report bad exit code if allowable duplications', function() {
+    var output = reportResults(1, {
+      '123': {
+        hash: '123',
+        children: [],
+        locations: ['foo.js:1', 'bar.js:2'],
+        source: 'var a = 1;'
+      }
+    });
+
+    expect(output.exitCode).to.equal(0);
   });
 });
