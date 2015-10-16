e4031a0666c9f5b6ebf4c145ea554944289b5446
Andrew Kiellor <akiellor@gmail.com>
2015-10-16 00:26:43 -0700

    Added another broad hash test.

--- test/hash_ast_spec.js
+++ test/hash_ast_spec.js
@@ -1,13 +1,29 @@
 var expect = require('chai').expect;
+var falafel = require('falafel');
 var hashAst = require(__dirname + '/../hash_ast');
 var generate = require(__dirname + '/../generate');
 
 describe('hash ast', function() {
   for (var i = 0; i < 15; i++) {
-    it('ast hashed twice should be the same', function() {
+    it('ast hashed twice should be the same ' + i, function() {
       var result = generate('FunctionDeclaration');
 
       expect(hashAst('stdin', result)).to.deep.equal(hashAst('stdin', result));
+    });
+  }
+
+  for (var i = 0; i < 15; i++) {
+    it('hash of child should exist in results for root ' + i, function() {
+      var result = generate('FunctionDeclaration');
+      var rootKeys = Object.keys(hashAst('stdin', result));
+
+      falafel(result, function(node) {
+        var childKeys = Object.keys(hashAst('stdin', node.source()));
+
+        childKeys.forEach(function(key) {
+          expect(rootKeys).to.contain(key);
+        });
+      });
     });
   }
 });
