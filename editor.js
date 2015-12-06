595ef9409ba7232e0e91bd1352b9b8ba0a1d8735
Andrew Kiellor <akiellor@gmail.com>
2015-12-05 21:26:29 -0800

    Allow for skipping and opening file at location.

--- editor.js
+++ editor.js
@@ -2,10 +2,10 @@ import child_process from 'child_process';
 import fs from 'fs';
 import csp from 'js-csp';
 
-export function editor(file) {
+export function editor(file, location) {
   var done = csp.chan();
 
-  var vim = child_process.spawn('vim', [file], {
+  var vim = child_process.spawn('vim', ['+' + location, file], {
     stdio: 'inherit'
   });
 
