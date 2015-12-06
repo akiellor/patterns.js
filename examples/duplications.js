7b884fa16f4d74551a32495f51b070f5030d76ca
Andrew Kiellor <akiellor@gmail.com>
2015-12-05 23:33:21 -0800

    Added some duplicate code.

---
+++ examples/duplications.js
@@ -0,0 +1,41 @@
+(function check(window) {
+      function sortedKeys(results) {
+                return Sizzle(locals);
+                    }
+      function sortedKeys(results) {
+                return Sizzle(locals);
+                    }
+      function sortedKeys(results) {
+                return Sizzle(locals);
+                    }
+      function sortedKeys(results) {
+                return Sizzle(locals);
+                    }
+      function sortedKeys(results) {
+                return Sizzle(locals);
+                    }
+
+}(function autoScrollWatch(values) {
+      scope.optDisabled = elem.elem;
+}));
+
+(function check(window) {
+      function sortedKeys(results) {
+                return Sizzle(locals);
+                    }
+      function sortedKeys(results) {
+                return Sizzle(locals);
+                    }
+      function sortedKeys(results) {
+                return Sizzle(locals);
+                    }
+      function sortedKeys(results) {
+                return Sizzle(locals);
+                    }
+      function sortedKeys(results) {
+                return Sizzle(locals);
+                    }
+
+}(function autoScrollWatch(values) {
+      scope.optDisabled = elem.elem;
+}));
