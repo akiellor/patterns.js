1489da13ba12c68f87004215d9478447c730eb85
Andrew Kiellor <akiellor@gmail.com>
2015-12-07 10:08:23 -0800

    Migrated refactor_inerpolation_spec to mocha.

--- test/refactor_interpolation_spec.js
+++ test/refactor_interpolation_spec.js
@@ -1,32 +1,32 @@
-#!/usr/bin/env babel-node --harmony
-
 import escodegen from 'escodegen';
 import parser from '../parser.js';
 import refactorInterpolation from '../lib/refactor_interpolation.js';
 import chai from 'chai';
 const expect = chai.expect;
 
+describe('refactor to interpolation', function() {
+  [
+    ['"1" + "2"', '`12`'],
+    ['"1" + foo', '`1${ foo }`'],
+    ['foo + "1"', '`${ foo }1`'],
+    ['foo() + "1"', '`${ foo() }1`'],
+    ['foo + " " + bar + " " + baz', '`${ foo } ${ bar } ${ baz }`'],
+    ['"" + string', '`${ string }`'],
+    ['prefix + ":" + node.type + ":" + node.start + ":" + node.end',
+      '`${ prefix }:${ node.type }:${ node.start }:${ node.end }`'],
+      ['"Could not parse: " + name + " " + e.message',
+        '`Could not parse: ${ name } ${ e.message }`'],
+        ['a + b + c', '`${ a }${ b }${ c }`']
+  ].forEach(function(expectation) {
+    const [ input, output ] = expectation;
+    it(`should refactor ${input} to ${output}`, function() {
+      const expression = parser.parse(input).body[0].expression;
 
-[
-  ['"1" + "2"', '`12`'],
-  ['"1" + foo', '`1${ foo }`'],
-  ['foo + "1"', '`${ foo }1`'],
-  ['foo() + "1"', '`${ foo() }1`'],
-  ['foo + " " + bar + " " + baz', '`${ foo } ${ bar } ${ baz }`'],
-  ['"" + string', '`${ string }`'],
-  ['prefix + ":" + node.type + ":" + node.start + ":" + node.end',
-   '`${ prefix }:${ node.type }:${ node.start }:${ node.end }`'],
-  ['"Could not parse: " + name + " " + e.message',
-   '`Could not parse: ${ name } ${ e.message }`'],
-  ['a + b + c', '`${ a }${ b }${ c }`']
-].forEach(function(expectation) {
-  const [ input, output ] = expectation;
-  const expression = parser.parse(input).body[0].expression;
+      const refactored = refactorInterpolation(expression);
 
-  const refactored = refactorInterpolation(expression);
-
-  expect(escodegen.generate(refactored)).to.equal(output);
+      expect(escodegen.generate(refactored)).to.equal(output);
+    });
+  });
 });
-
 
 
